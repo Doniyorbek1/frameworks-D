c670496e5a0f3aec3b6e742d6d00390c796c6e20
Doniyorbek <programmmerqety1@gmail.com>
2024-11-26 10:12:30 +0500

    write ","

--- wsgi/main.py
+++ wsgi/main.py
@@ -1,6 +1,6 @@
 from wsgiref.simple_server import make_server
 
-def sample_app(environ start_response):
+def sample_app(environ, start_response):
     status = "200 OK"
     headers = [("Content-type", "text/plain")]
     start_response(status, headers)
